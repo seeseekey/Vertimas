e381f6bb3914d052cacd25f818843245cd7e3503
seeseekey <seeseekey@729e799f-5207-0aa4-7256-7b19a4316497>
2011-02-23 16:44:14 +0000

    git-svn-id: https://vertimas.googlecode.com/svn/trunk@3 729e799f-5207-0aa4-7256-7b19a4316497

---
+++ Vertimas/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vertimas
+{
+    public static class Common
+    {
+        public static string GetDefaultValue(string key)
+        {
+            return string.Format(
+                Properties.Settings.Default.NonTranslatedString,
+                key, key.ToUpper(), key.ToLower());
+        }
+
+    }
+}
---
+++ Vertimas/ZoomWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vertimas
+{
+    public partial class ZoomWindow : Form
+    {
+        public ZoomWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
